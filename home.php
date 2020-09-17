056821c47df8bf5b61c2487fdb3987450773c4a1
Itallo Vidal <itallovidalll@gmail.com>
2020-09-17 03:19:34 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://kit.fontawesome.com/782e4cfd7d.js" crossorigin="anonymous"></script>
+    <title>ACS Cuidadores</title>
+</head>
+<body>
+
+    <main>
+        
+        <!-- banner inicial  -->
+        <section id="banner">
+            <img src="imagens/m3.jpg" alt="">
+
+            <span> Trabalhando com muito <br> carinho, amor, respeito e <br> responsabilidade.</span>
+        </section>
+
+        <!-- barra de nav do site -->
+        <nav id="container-navbar">
+            <ul id="navbar">
+                <a class="navbar-item" href="#banner"> Início                  </a>
+                <a class="navbar-item" href="#sobresobre"> Sobre nós             </a>
+                <a class="navbar-item" href="#serv"> Serviços               </a>
+                <a class="navbar-item" href="#container-contato"> Contato   </a>
+                <a class="navbar-item" href="#parceiros"> Parceiros            </a>
+            </ul>
+
+            <img src="imagens/logo.jpeg" alt="">
+        </nav>
+
+        <!-- parte do sobre, servicos -->
+
+        <section class="topico">
+            <h1> O que fazemos e quem somos?!</h1>
+            
+            <img src="imagens/m1.jpg" alt="">
+
+        </section>
+
+        <section class="sobre">
+            
+            <p id="sobresobre">
+                &ThinSpace; &ThinSpace; &ThinSpace; O <i> cuidador de idoso dependente </i> é aquele que se dedica, dispondo-se e dispondo do seu tempo, dando atenção total e especial a um idoso, 
+                o ajudando na sua incapacidade e limitação, acompanhando-o diariamente pela sua rotina a fim de minimizar seus esforços em qualquer atividade e fazer companhia. Essa pessoa assume e se 
+                responsabiliza pelas tarefas de cuidado, sem contar, na maioria das vezes, com a ajuda de profissionais.
+            </p>
+
+            <p>
+                &ThinSpace; &ThinSpace; &ThinSpace; Oferecemos assistência aos nossos pacientes da maneira mais humanizada e agregadora possível. Nós buscamos, com muito amor, atenção, respeito, responsabilidade 
+                - e tudo o que há direito - cuidar dos nosso clientes, por sabermos que cada um vale muito. Em dois anos de trajetória, nossos serviços nos proporcionaram momentos incríveis e amigos que confiam no 
+                nosso trabalho e sabem do nosso profissionalismo e diligência.
+            </p>
+
+            <p>
+                &ThinSpace; &ThinSpace; &ThinSpace; Um dos grandes diferenciais da empresa é o atendimento personalizado. Sempre estamos prontos para atendermos todas as famílias com dedicação, 
+                foco e empatia 24 horas por dia, 7 dias por semana, nos 365 dias do ano. Nosso trabalho é fruto de muito empenho e nos orgulhamos disso. A equipe é zelosa, experiente e capacitada para 
+                executar os serviços de assistência domiciliares e hospitalares com máxima eficiência.
+            </p>
+
+        </section>
+
+        <section class="topico">
+            <h1 id="especialidade"> Nossa especialidade </h1>
+            
+            <img id="especialidade-pic" src="imagens/m4.jpeg" alt="">
+        </section>
+
+        <section class="sobre" id="serv">
+
+            <p>
+                Se você tem alguma dúvida ou quer mais informações, nossos consultores estão a disposição para lhe ajudar. Nosso serviço é extremamente importante para as famílias que necessitam de um cuidado especial para com seus familiares.
+                Estamos à disposição para melhor atendê-los para quem fiquem tranquilos em relação a quaisquer dúvidas e afins. Nossa principal função é melhor cuidar de  
+                determinado idoso, desta forma, atendemos suas necessidades tais quais: 
+            </p>
+
+            <h2> Higiene, Companhia, Realizar atividade domésticas </h2>
+
+            <ul>
+                <li> Banho </li>
+                <li> Fraldas e aparelhos </li>
+                <li> Escovar dentes </li>
+                <li> Manter unhas limpas</li>
+                <li> Cortar barba </li>
+                <li> Controlar medicação </li>
+            </ul>
+        </section>
+
+
+        <!-- parte do contato -->
+        <section id="container-contato">
+
+            <article id="contato">
+                <h1 class="contato" >Esperamos por você! </h1>
+
+                <p class="contato">
+                    &ThinSpace; &ThinSpace; &ThinSpace; Estamos prontos para melhor atendê-lo. Estamos à disposição no Instagram, Email,  Facebook e WhatsApp! Se restou alguma dúvida, ou Cuidadores
+                    não tenha ficado alguma coisa clara, ou apenas quer mais informações, nossos consultores estarão esperando por você. Acesse abaixo a plataforma de sua preferência 
+                    e venha se juntar a nossa família. 
+                </p>
+
+                <p class="contato icons">
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-facebook"></i>
+                    <i class="fab fa-whatsapp"></i>
+                    <i class="fas fa-envelope"></i>
+                </p>
+            </article>
+
+            <article id="contato-foto">
+                <img src="imagens/m2.jpg" alt="">
+            </article>
+
+        </section>
+
+        <footer>
+            <div id="parceiros">
+                <h1> Parceiros </h1>
+
+                <img src="imagens/famoza.png" alt="">
+                <img src="imagens/webradio.png" alt="">
+            </div>
+
+            <div id="emprego">
+                <h1> Junte-se a nós!</h1>
+
+                <p> Sempre há mais espaço para quem está disposto a amar! <br> Venha participar da nossa equipe, entre em contato conosco <br> pelas redes fornecidas a cima.  Estamos no aguardo!</p>
+            </div>
+        </footer>
+    </main>
+    
+</body>
+</html>
